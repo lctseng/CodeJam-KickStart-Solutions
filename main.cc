dc7ac622621157b376ae2fe2fd7a2fda074379b5
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-18 12:32:06 +0800

    add skeleton main

---
+++ main.cc
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int T;
+  cin >> T;
+  for (int t = 1; t <= T; t++) {
+    // TODO: read input for each case and compute output
+    cout << "Case #" << t << ": " << 123 << endl;
+  }
+  return 0;
+}
