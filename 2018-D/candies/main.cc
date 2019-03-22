297ac4c0c0c95452d73e2f45632e43d7bb181992
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-22 13:11:26 +0800

    2018-D-1: candies: pass large

--- 2018-D/candies/main.cc
+++ 2018-D/candies/main.cc
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <set>
 #include <string>
 #include <vector>
 using namespace std;
@@ -10,7 +11,79 @@ using namespace std;
 //     return nullptr;
 // }( );
 
-namespace Simple {
+// algo
+// when advancing j, record every curD and store in tree
+// when finding best ending, use tree search find the maxinum valid one
+// when advancing i, erase '1' tree it with value S[i], decrease all by S[i]
+
+namespace Large {
+
+string solveSlidingWindow(int64_t N, int64_t O, int64_t D, vector<int> &S) {
+  // cout << "S: ";
+  // for (int val : S) {
+  //   cout << val << ' ';
+  // }
+  // cout << endl;
+  // simple case: all candy are positive
+  // use sliding winodw!
+  int64_t curO = 0, curD = 0;
+  int i = 0, j = 0;
+  // i: leftmost index meet condition
+  // j: first index that breaks condition
+  bool found = false;
+  int64_t maxValue = 0;
+  // extra, BE CAREFUL the data type
+  multiset<int64_t> tree;
+  int64_t offset = 0;
+  while (i < N) {
+    while (j < N && (S[j] % 2 == 0 || curO + 1 <= O)) {
+      // advance!
+      curO += S[j] & 1;
+      curD += S[j];
+      // cout << "using number: " << S[j] << endl;
+      tree.insert(curD - offset);
+      j++;
+    }
+    // record if non-empty
+    // cout << "current tree size: " << tree.size() << endl;
+    // for (int val : tree) {
+    //   cout << val + offset << ' ';
+    // }
+    // cout << endl;
+    if (tree.size()) {
+      auto overIt = tree.upper_bound(D - offset);
+      if (overIt != tree.begin()) {
+        --overIt;
+        int64_t treeMaxValue = *overIt + offset;
+        if (!found || treeMaxValue > maxValue) {
+          found = true;
+          maxValue = treeMaxValue;
+        }
+      }
+    }
+    if (tree.size()) {
+      // advance i by 1
+      // update tree
+      tree.erase(tree.find(S[i] - offset));
+      offset -= S[i];
+      // update change from i
+      curO -= S[i] & 1;
+      curD -= S[i];
+      ++i;
+      // dont need to update changes from J!
+      // will be check next loop
+    } else {
+      // handle empty case
+      i = j = j + 1;
+    }
+  }
+  // return string!
+  return found ? to_string(maxValue) : "IMPOSSIBLE";
+}
+
+} // namespace Large
+
+namespace Small {
 
 string solveSlidingWindow(int64_t N, int64_t O, int64_t D, vector<int> &S) {
   // simple case: all candy are positive
@@ -81,7 +154,7 @@ string solveEnumerate(int64_t N, int64_t O, int64_t D, vector<int> &S) {
   // return string!
   return found ? to_string(maxValue) : "IMPOSSIBLE";
 }
-} // namespace Simple
+} // namespace Small
 
 vector<int> GenerateS(int64_t N, int64_t X1, int64_t X2, int64_t A, int64_t B,
                       int64_t C, int64_t M, int64_t L) {
@@ -107,7 +180,7 @@ int main() {
     cin >> X1 >> X2 >> A >> B >> C >> M >> L;
     auto S = GenerateS(N, X1, X2, A, B, C, M, L);
 
-    cout << "Case #" << t << ": " << Simple::solveSlidingWindow(N, O, D, S)
+    cout << "Case #" << t << ": " << Large::solveSlidingWindow(N, O, D, S)
          << endl;
   }
   return 0;
