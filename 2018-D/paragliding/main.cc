966c3c7beb6deb76169a7b9bfb781f8a73ecf9b0
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-22 18:47:01 +0800

    2018-D-2: paragliding: pass large

--- 2018-D/paragliding/main.cc
+++ 2018-D/paragliding/main.cc
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <deque>
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -8,6 +9,69 @@ using namespace std;
 //     std::cin.tie( nullptr );
 //     return nullptr;
 // }( );
+
+// algo:
+
+namespace Large {
+
+struct Point {
+  int x, y;
+};
+
+int solve(int N, int K, vector<int> &P, vector<int> &H, vector<int> &X,
+          vector<int> &Y) {
+  int count = 0;
+  auto cmpPointLess = [](const Point &a, const Point &b) { return a.x < b.x; };
+  // use 'height relative to origin'
+  vector<Point> towers;
+  for (int i = 0; i < N; i++) {
+    towers.push_back({P[i], H[i]});
+  }
+  // sort tower according to x
+  sort(towers.begin(), towers.end(), cmpPointLess);
+  int leftOffset = towers.front().x;
+  int rightOffset = towers.back().x;
+  // compute towerHeightLeft
+  vector<int> towerHeightLeft(N);
+  vector<int> towerHeightRight(N);
+  for (int i = 0; i < N; i++) {
+    auto &tower = towers[i];
+    int prevValue = i > 0 ? towerHeightLeft[i - 1] : 0;
+    towerHeightLeft[i] = max(tower.y + (tower.x - leftOffset), prevValue);
+  }
+  for (int i = N - 1; i >= 0; i--) {
+    auto &tower = towers[i];
+    int prevValue = i < N - 1 ? towerHeightRight[i + 1] : 0;
+    towerHeightRight[i] = max(tower.y + (rightOffset - tower.x), prevValue);
+  }
+  for (int i = 0; i < K; i++) {
+    // search from left
+    auto leftStartIt =
+        upper_bound(towers.begin(), towers.end(), Point{X[i], 0}, cmpPointLess);
+    if (leftStartIt != towers.begin()) {
+      int towerIndex = leftStartIt - towers.begin() - 1;
+      if (towerHeightLeft[towerIndex] >= (Y[i] + X[i] - leftOffset)) {
+        // this ballon count!
+        count++;
+        continue; // go to next ballon
+      }
+    }
+    // search from right
+    auto rightStartIt =
+        lower_bound(towers.begin(), towers.end(), Point{X[i], 0}, cmpPointLess);
+    if (rightStartIt != towers.end()) {
+      int towerIndex = rightStartIt - towers.begin();
+      if (towerHeightRight[towerIndex] >= (Y[i] + rightOffset - X[i])) {
+        // this ballon count!
+        count++;
+        continue; // go to next ballon
+      }
+    }
+  }
+  return count;
+}
+
+} // namespace Large
 
 namespace Small {
 int solve(int N, int K, vector<int> &P, vector<int> &H, vector<int> &X,
@@ -41,11 +105,6 @@ vector<int> Generate(int size, int64_t v1, int64_t v2, int64_t A, int64_t B,
     v1 = v2;
     v2 = v;
   }
-  // cout << "Generated: ";
-  // for (int val : res) {
-  //   cout << val << ' ';
-  // }
-  // cout << endl;
   return res;
 }
 
@@ -66,7 +125,7 @@ int main() {
     cin >> v1 >> v2 >> A >> B >> C >> M;
     vector<int> Y = Generate(K, v1, v2, A, B, C, M);
 
-    cout << "Case #" << t << ": " << Small::solve(N, K, P, H, X, Y) << endl;
+    cout << "Case #" << t << ": " << Large::solve(N, K, P, H, X, Y) << endl;
   }
   return 0;
 }
