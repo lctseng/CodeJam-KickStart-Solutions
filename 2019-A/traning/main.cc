5fc477f9dcecd79dda9d9f09b4e345a54e0ab646
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2019-03-24 15:28:57 +0800

    2019-A-1 training: pass all

--- 2019-A/traning/main.cc
+++ 2019-A/traning/main.cc
@@ -19,12 +19,48 @@
 
 using namespace std;
 
+namespace Small {
+int solve(int N, int P, vector<int> &students) {
+  sort(students.begin(), students.end());
+  // collect P
+  int sum = 0;
+  for (int i = 0; i < P - 1; i++) {
+    sum += students[P - 1] - students[i];
+  }
+  int minSum = sum;
+  // consider  from P-th
+  for (int i = P; i < N; i++) {
+    // reject
+    sum -= students[i - 1] - students[i - P];
+    sum += (P - 1) * (students[i] - students[i - 1]);
+    if (sum < minSum)
+      minSum = sum;
+  }
+  // sub
+  return minSum;
+}
+} // namespace Small
+
+namespace Large {
+int solve() {
+  // TODO
+  return 0;
+}
+} // namespace Large
+
 int main() {
   int T;
   cin >> T;
   for (int t = 1; t <= T; t++) {
-    // TODO: read input for each case and compute output
-    cout << "Case #" << t << ": " << 123 << endl;
+    int N, P;
+    cin >> N >> P;
+    vector<int> students;
+    for (int i = 0; i < N; i++) {
+      int s;
+      cin >> s;
+      students.push_back(s);
+    }
+    cout << "Case #" << t << ": " << Small::solve(N, P, students) << endl;
   }
   return 0;
 }
